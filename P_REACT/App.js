f416a7ab36e839fc983ef718c7106400ed8a0aba
Wallace Sousa <wallaceinaciogs@gmail.com>
2022-04-26 20:42:05 -0300

    add 26.4.22

--- P_REACT/App.js
+++ P_REACT/App.js
@@ -1,12 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, SafeAreaView, Image, Dimensions } from 'react-native';
+import topo from './assets/assets/topo.png'
+
+const width = Dimensions.get('screen').width;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Wallace Sousa 21:16</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.topo} source={topo} />
+      <Text style={styles.tituloCarrinho}>Carrinho</Text>
+      <Text>Detalhes do carrinho</Text>
+      <Text>Kit IoT</Text>
+      <Text>Nome do Vendedor</Text>
+      <Text>Um kit IoT que revoluciona a vida dos estudantes</Text>
+      <Text>R$ 99,00</Text>
+      <StatusBar />
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +22,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  topo:{
+    width:"100%",
+    height:582/772 * width
+  },
+  tituloCarrinho:{
+    position:'absolute',
+    paddingVertical: 60,
+    textAlign:"center",
+    width:'100%',
+    fontSize: 20,
+    fontWeight: "bold",
+    lineHeight: 40
+  }
 });
